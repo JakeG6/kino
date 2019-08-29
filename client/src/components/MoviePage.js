5d398e4998aa2644a678be4a7358e11e117ba6e4
Jacob <jakeguss@gmail.com>
2019-08-28 22:20:57 -0600

    added api call to MoviePage component

--- client/src/components/MoviePage.js
+++ client/src/components/MoviePage.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const MoviePage = ({match}) => {
 
+    const [pageMovie, setPageMovie] = useState([])
+
+    useEffect(() => {
+        //this is having issues
+        axios.get(`https://api.themoviedb.org/3/movie/${match.params.id}?api_key=3f1b30e6df7ae6dcf64dc94b36c9487d&language=en-US`)
+            .then(res => {
+                console.log(res.data)
+                setPageMovie(res.data)
+        })
+
+    })
+
     return (
         <div>
-            movie page: {match.params.id}
+            <p>movie id: {match.params.id}</p>
+            <p>name: {pageMovie.original_title}</p>
         </div>
     )
 }
