f3f7a48c5597aab7212ca33ed0d1bca96d094587
Jacob <jakeguss@gmail.com>
2019-10-18 15:57:29 -0600

    moving around useEffect

--- client/src/components/SearchResultsPage/SearchResultsPage.js
+++ client/src/components/SearchResultsPage/SearchResultsPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Accordion from 'react-bootstrap/Accordion'
 
+import Button from 'react-bootstrap/Button'
 import axios from 'axios';
 
 import Container from 'react-bootstrap/Container';
@@ -17,30 +17,37 @@ const apiKey = '3f1b30e6df7ae6dcf64dc94b36c9487d';
 
 const SearchResultsPage = () => {
 
-    const [searchResults, setSearchResults] = useState(null)
+    const [searchResults, setSearchResults] = useState(null);
+    const [pageCount, setPageCount] = useState(1);
 
     //Deliver user search results from TMDB api
     useEffect(() => {
 
-        const fetchResults = async () => {
+        const fetchFirstResults = async () => {
+            console.log(pageCount)
+            const apiResults = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=batman&page=${pageCount}&include_adult=false`);
 
-            const apiResults = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=batman&page=1&include_adult=false`)
+            //console.log(apiResults);
+            await setSearchResults(apiResults.data.results);
+            console.log('good will')
+        }   
 
-            console.log(apiResults)
-
-            setSearchResults(apiResults.data.results)
-
-            console.log(searchResults)
+        if (searchResults == null) {
+            console.log('getting the first results')
+            fetchFirstResults();
         }
 
-        fetchResults();
+    
 
     }, [])
 
-    //go to movie's page when profile is clicked on
-    // const goToMovie = (id) =>  {
-    //     history.push(`/movies/${id}`);
-    // }
+        const showMoreResults = async () => {
+
+            await setPageCount(pageCount + 1);
+            const apiResults = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=batman&page=${pageCount}&include_adult=false`);
+            await setSearchResults([...searchResults, ...apiResults.data.results]);
+            console.log(searchResults);
+        }
 
     if (searchResults) {
         return (
@@ -62,22 +69,22 @@ const SearchResultsPage = () => {
                         <Col xs={12}>
                             <ListGroup>
                                 {searchResults.map(result => (
-                                    
-                                        <ListGroup.Item >
-                                            <Row>
-                                                <Col sm={4}>
-                                                    <Image src={`http://image.tmdb.org/t/p/w92${result.poster_path}`} rounded />
-                                                </Col>
-                                                <Col sm={8}>
-                                                    <Link to={`/movie/${result.id}`}><b>{result.title} ({result.release_date.slice(0, 4)})</b></Link>
-                                                    <i>{result.overview}</i>
-                                                </Col>
-                                            </Row>
-                                        </ListGroup.Item>
-                                    
-                                    
+                                    <ListGroup.Item >
+                                        <Row>
+                                            <Col sm={4}>
+                                                <Image src={`http://image.tmdb.org/t/p/w92${result.poster_path}`} rounded />
+                                            </Col>
+                                            <Col sm={8}>
+                                                <Link to={`/movie/${result.id}`}><b>{result.title} ({result.release_date.slice(0, 4)})</b></Link>
+                                                <i>{result.overview}</i>
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
                                 ))}
                             </ListGroup>
+                            <Button onClick={showMoreResults} variant="primary" size="lg" block>
+                                Show More Results
+                            </Button>
                         </Col>
                     </Row>
                 </Container>
