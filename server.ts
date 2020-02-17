0c4da84390d25c4ed26de5e1d68ea2df445bf63b
Jacob <jakeguss@gmail.com>
2020-02-16 19:05:40 -0700

    added node modules to .gitignore. tsconfig added

---
+++ server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+//Kino frontend
+app.get('/', (req, res) => res.send('frontend will be here'))
+
+
+app.get('/api/hello', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Kino API running on ${port}.`))
